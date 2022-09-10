1160aac4b217eac75adbbb0ca22ac1a6642c76be
mat hayward-hill <mhh@DESKTOP-FN6IF17>
2022-09-10 15:06:32 +0100

    Revert "add securitygroups tf module"
    This reverts commit 43494ac1434743a9ea66d888541d4619198c7031.

--- terraform/modules/EMR/input.tf
+++ terraform/modules/EMR/input.tf
@@ -1,7 +1,3 @@
 variable subnet_id {}
 
 variable key_name {}
-
-variable master_sg_id {}
-
-variable worker_sg_id {}
--- terraform/modules/VPC/output.tf
+++ terraform/modules/VPC/output.tf
@@ -1,8 +1,3 @@
-output vpc_id {
-  value = aws_vpc.my-vpc.id
-}
-
-
 output subnet_id {
   value = aws_subnet.mypublicsubnet.id
 }
