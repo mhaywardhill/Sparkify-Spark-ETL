43494ac1434743a9ea66d888541d4619198c7031
mat hayward-hill <mhh@DESKTOP-FN6IF17>
2022-09-10 14:59:12 +0100

    add securitygroups tf module

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -8,9 +8,19 @@ module "VPC" {
 	pubsubcidr	= var.pubsubcidr
 }
 
-module "EMR" {
-	source	= "../../modules/EMR"
-	subnet_id	= module.VPC.subnet_id
-	key_name	= var.key_name
-	depends_on = [module.VPC]
+module "securitygroups" {
+	source	= "../../modules/securitygroups"
+	vpc_id	= module.VPC.vpc_id
+	depends_on 	= [module.VPC]
 }
+
+module "EMR" {
+	source		= "../../modules/EMR"
+	subnet_id		= module.VPC.subnet_id
+	master_sg_id	= module.securitygroups.master_sg_id
+	worker_sg_id	= module.securitygroups.worker_sg_id
+	key_name		= var.key_name
+	depends_on 		= [module.securitygroups]
+					
+}
+
--- terraform/modules/EMR/input.tf
+++ terraform/modules/EMR/input.tf
@@ -1,3 +1,7 @@
 variable subnet_id {}
 
 variable key_name {}
+
+variable master_sg_id {}
+
+variable worker_sg_id {}
