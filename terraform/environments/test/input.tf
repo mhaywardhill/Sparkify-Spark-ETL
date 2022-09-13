ca43234af2d62f75e5a600c1cb728363d665cdf7
mhaywardhill <haywardhill@gmail.com>
2022-09-13 17:58:11 +0100

    update CIDR for VPC and subnet

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,15 +1,19 @@
+# IP Address range assigned to the VPC, 216 assignable hosts
 variable vpc-cidr {
-	default = "172.168.0.0/16"
+	default = "10.16.0.0/24"
 }
 
+# IP Address range assigned to the Subnet, 30 assignable hosts
 variable pubsubcidr {
-	default = "172.168.0.0/24"
+	default = "10.16.0.0/27"
 }
 
+# The name of the EC2 Key (with the file extension)
 variable key_name {
 	sensitive   = true
 }
 
+# Used to add inbound rule to the EMR master node security group
 variable my_public_ip {
 	sensitive   = true
 }
